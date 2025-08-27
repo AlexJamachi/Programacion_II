81145ed5705d02550153a981309fee114e218730
AlexJamachi <alexjamachi2.c@gmail.com>
2025-08-27 03:14:28 -0400

    Update EcuacionCuadratica.java

--- Practica 1/Ej2_Ecuaciones_Cuadraticas/EcuacionCuadratica.java
+++ Practica 1/Ej2_Ecuaciones_Cuadraticas/EcuacionCuadratica.java
@@ -6,19 +6,19 @@ public class EcuacionCuadratica {
         this.b = b;
         this.c = c;
     }
-    public double obtenerDiscriminante() {
+    public double getDiscriminante() {
         return b * b - 4 * a * c;
     }
-    public double obtenerRaiz1() {
-        if (obtenerDiscriminante() >= 0) {
-            return (-b + Math.sqrt(obtenerDiscriminante())) / (2 * a);
+    public double getRaiz1() {
+        if (getDiscriminante() >= 0) {
+            return (-b + Math.sqrt(getDiscriminante())) / (2 * a);
         } else {
             return 0;
         }
     }
-    public double obtenerRaiz2() {
-        if (obtenerDiscriminante() >= 0) {
-            return (-b - Math.sqrt(obtenerDiscriminante())) / (2 * a);
+    public double getRaiz2() {
+        if (getDiscriminante() >= 0) {
+            return (-b - Math.sqrt(getDiscriminante())) / (2 * a);
         } else {
             return 0;
         }
