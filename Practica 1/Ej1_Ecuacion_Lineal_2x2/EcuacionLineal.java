4f2ee4ef8edb1b84fc29ec143dfde777ca02c05d
AlexJamachi <alexjamachi2.c@gmail.com>
2025-08-27 00:40:36 -0400

    Resolucion de Practica 1 por Jamachi Condori Alex Arturo

---
+++ Practica 1/Ej1_Ecuacion_Lineal_2x2/EcuacionLineal.java
@@ -0,0 +1,21 @@
+package Ej1_Ecuacion_Lineal_2x2;
+public class EcuacionLineal {
+    private double a, b, c, d, e, f;
+    public EcuacionLineal(double a, double b, double c, double d, double e, double f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+    public boolean tieneSolucion() {
+        return (a * d - b * c) != 0;
+    }
+    public double obtenerX() {
+        return (e * d - b * f) / (a * d - b * c);
+    }
+    public double obtenerY() {
+        return (a * f - e * c) / (a * d - b * c);
+    }
+}
---
+++ Practica 1/Ej2_Ecuaciones_Cuadraticas/PruebaEcuacionCuadratica.java
@@ -0,0 +1,21 @@
+package Ej2_Ecuaciones_Cuadraticas;
+import java.util.Scanner;
+public class PruebaEcuacionCuadratica {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese a, b, c: ");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        EcuacionCuadratica ecuacion = new EcuacionCuadratica(a, b, c);
+        double discriminante = ecuacion.getDiscriminante();
+        if (discriminante > 0) {
+            System.out.println("La ecuación tiene dos raíces " + ecuacion.getRaiz1() + " y " + ecuacion.getRaiz2());
+        } else if (discriminante == 0) {
+            System.out.println("La ecuación tiene una raíz " + ecuacion.getRaiz1());
+        } else {
+            System.out.println("La ecuación no tiene raíces reales");
+        }
+        scanner.close();
+    }
+}
